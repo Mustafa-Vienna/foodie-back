08b832f6bed4c5b8c8abbf993387a8315d084c98
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-01-21 20:34:21 +0000

    added posts app with serializers

---
+++ posts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+  author = serializers.ReadOnlyField(source = 'author.username')
+  is_author = serializers.SerializerMethodField()
+  profile_id = serializers.ReadOnlyField(source = 'author.profile.id')
+  profile_image = serializers.ReadOnlyField(source = 'author.profile.image.url')
+  
+  def get_is_author(self, obj):
+    request = self.context('request')
+    return request.user == obj.author
+  
+  class Meta:
+    model = Post
+    fields = [
+      'id', 'author', 'is_author', 'profile_id',
+      'profile_image', 'created_at', 'updated_at',
+      'title', 'content', 'image'
+    ]
