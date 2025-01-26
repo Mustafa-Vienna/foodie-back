1ed929d0b70711b748c836d2e13596760b6fec17
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-01-26 17:29:27 +0000

    Add filtering with Django Filters to views, settings, and requirements.txt

--- foodie_api/settings.py
+++ foodie_api/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'django_filters',
     
     'profiles',
     'posts',
@@ -137,3 +138,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,15 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from foodie_api.permissions import IsOwnerOrReadOnly
 from posts.models import Post, Tag
 from posts.serializers import TagSerializer, PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostListCreateView(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['category', 'tags']
   
   def perform_create(self, serializer):
     serializer.save(author=self.request.user)
