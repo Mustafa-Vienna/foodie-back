7ce1a184ae30534485e4640420de2a6a3ce732a2
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-01-30 18:15:53 +0000

    Added a root route in the project views and update urls accordingly

--- foodie_api/urls.py
+++ foodie_api/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
+    path("", root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
