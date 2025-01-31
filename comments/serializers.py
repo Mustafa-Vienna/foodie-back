c980dcbd51001e621945875907342f124d44e51f
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-01-31 17:41:13 +0000

    Added created_at and updated_at fields with humanized formatting in CommentSerializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -8,16 +9,24 @@ class CommentSerializer(serializers.ModelSerializer):
   is_author = serializers.SerializerMethodField()
   profile_id = serializers.ReadOnlyField(source='author.profile.id')
   profile_image = serializers.ReadOnlyField(source='author.profile.image.url')
+  created_at = serializers.SerializerMethodField()
+  updated_at = serializers.SerializerMethodField()
   
   def get_is_author(self, obj):
     request = self.context['request']
     return request.user == obj.author
   
+  def get_created_at(self, obj):
+    return naturaltime(obj.created_at)
+  
+  def get_updated_at(self, obj):
+    return naturaltime(obj.updated_at)
+  
   class Meta:
     model = Comment
     fields = [
       'id', 'author', 'is_author', 'profile_id',
-      'post', 'profile_image', 'created_at',
+      'profile_image', 'post','created_at',
       'updated_at', 'content', 
     ]
     
