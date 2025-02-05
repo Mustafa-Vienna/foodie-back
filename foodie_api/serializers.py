29cc8e5405c70e3416a1ed6219a24daf46c798fa
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-05 19:14:09 +0000

    feat(auth): Add email field to user registration serializer

--- foodie_api/serializers.py
+++ foodie_api/serializers.py
@@ -1,5 +1,8 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
@@ -10,3 +13,18 @@ class CurrentUserSerializer(UserDetailsSerializer):
     fields = UserDetailsSerializer.Meta.fields + (
       'profile_id', 'profile_image'
     )
+    
+  class CustomeRegisterSerializer(RegisterSerializer):
+    email = serializers.EmailField(required=True)
+    
+    def validate_email(self, email):
+      
+      email = get_adapter.clean_email(email)
+      if email and self.Meta.model.objects.filter(email=email).exist():
+        raise serializers.ValidationError("A user with this email already exists.")
+      return email
+    
+  def save(self, request):
+    user = super().save(request)
+    setup_user_email(request, user, [])
+    return user
