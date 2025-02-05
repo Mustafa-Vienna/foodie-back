9add48dfdd3a58449ffde4ec4697dab09462ede5
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-05 19:42:28 +0000

    Added email validation in CustomRegisterSerializer.

--- foodie_api/serializers.py
+++ foodie_api/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+import re
 
+User = get_user_model()
 
 class CurrentUserSerializer(UserDetailsSerializer):
   profile_id = serializers.ReadOnlyField(source='profile.id')
@@ -15,13 +18,26 @@ class CurrentUserSerializer(UserDetailsSerializer):
     )
     
   class CustomeRegisterSerializer(RegisterSerializer):
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(
+      required=True,
+      max_length=80,
+      error_messages={
+        "required": "Email is required",
+        "invalid": "Enter a valid email address!",
+        "max_length": "Email must be less then 80 characters!",
+      },
+    )
     
-    def validate_email(self, email):
+    def validate_email(self, value):
       
-      email = get_adapter.clean_email(email)
-      if email and self.Meta.model.objects.filter(email=email).exist():
+      email = value.strip().lower()
+      
+      if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        raise serializers.ValidationError("Enter a valid address!")
+      
+      if User.objects.filter(email=email).exists():
         raise serializers.ValidationError("A user with this email already exists.")
+      
       return email
     
   def save(self, request):
