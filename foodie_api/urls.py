4b467f4247b9d5559f0c54b38d13091ee962133f
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-07 20:53:48 +0000

    Updated urls.py to include token refresh endpoint and fix app-specific URL patterns

--- foodie_api/urls.py
+++ foodie_api/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("", root_route),
@@ -26,9 +27,13 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
-    path('',include('comments.urls')),
-    path('',include('likes.urls')),
-    path('',include('followers.urls')),
+    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('profiles/', include('profiles.urls')),
+    path('posts/', include('posts.urls')),
+    path('comments/',include('comments.urls')),
+    path('likes/',include('likes.urls')),
+    path('followers/',include('followers.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
