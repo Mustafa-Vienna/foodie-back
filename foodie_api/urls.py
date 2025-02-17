02ea65e7de18ecfa762afd91cb59ef96ddf8c863
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-17 17:18:48 +0100

    Removed explicit path prefixes for app routes (profiles, posts, comments, likes, followers)

--- foodie_api/urls.py
+++ foodie_api/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for foodie_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -28,12 +12,9 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-    path('profiles/', include('profiles.urls')),
-    path('posts/', include('posts.urls')),
-    path('comments/',include('comments.urls')),
-    path('likes/',include('likes.urls')),
-    path('followers/',include('followers.urls')),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('',include('comments.urls')),
+    path('',include('likes.urls')),
+    path('',include('followers.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
