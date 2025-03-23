c687e0fb669c84635767783a985088b0c3b3ab8c
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-23 17:28:34 +0100

    Added delete-like-by-post endpoint to likes app URLs.

--- likes/urls.py
+++ likes/urls.py
@@ -6,4 +6,5 @@ app_name = 'likes'
 urlpatterns = [
     path('', views.LikeListCreateView.as_view(), name='like-list-create'),
     path('<int:pk>/', views.LikeDetailView.as_view(), name='like-detail'),
+    path('post/<int:post_id>/', views.LikeDeleteByPostView.as_view(), name='like-delete-by-post'),
 ]
