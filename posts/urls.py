1092d933445be122ee50e84bd0cc6956bd9a4056
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-25 19:57:55 +0100

    Add edit view and url

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.PostListCreateView.as_view(), name='post-list-create'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
     path('tags/', views.TagListCreateView.as_view(), name='tag-list-create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,12 @@
 from django.db.models import Count
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions, filters, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from foodie_api.permissions import IsOwnerOrReadOnly
 from posts.models import Post, Tag
 from posts.serializers import TagSerializer, PostSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-
 
 
 class PostListCreateView(generics.ListCreateAPIView):
@@ -64,6 +64,22 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     comments_count=Count('comments',distinct=True)
   ).order_by('-created_at')
   
+class PostEditView(generics.UpdateAPIView):
+  """
+  Edit a post if the user is the author
+  """
+  serializer_class = PostSerializer
+  permission_classes = [IsOwnerOrReadOnly]
+  queryset = Post.objects.all()
+  
+  def update(self, request, *args, **kwargs):
+    partial = kwargs.pop('partial', False)
+    instance = self.get_object()
+    serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    serializer.is_valid(raise_exception=True)
+    self.perform_update(serializer)
+    return Response(serializer.data)
+    
 class TagListCreateView(generics.ListCreateAPIView):
   """
   List all tags or create a tag.
